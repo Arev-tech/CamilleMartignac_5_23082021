1128745727ba3c60d7a29a50496bb55700db1f54
Camille Martignac <79940961+Arev-tech@users.noreply.github.com>
2021-08-31 12:26:06 +0200

    javascript

---
+++ js/script.js
@@ -0,0 +1,76 @@
+let url = 'http://localhost:3000/api/teddies';
+
+fetch(url)
+    .then((resp) => resp.json())
+    .then(function(data){
+        let array = data;
+        class Ours {
+            constructor(colors, id, name, price, imageUrl, description) {
+              this.colors = colors;
+              this.id = id;
+              this.name = name;
+              this.price = price;
+              this.imageUrl = imageUrl;
+              this.description = description;
+            }
+        
+        }
+        let nombreDOurs = 1;
+        let lengthOurs = data.length;
+        let divOurs = '';
+
+        function creationHREF(ours, n){
+            href = 'href="html/';
+            href += ours;
+            href += '-';
+            href += n;
+            href += '.html"';
+            return href;
+        }
+
+        function creationID(ours, n){
+            id = 'id="';
+            id += ours;
+            id += '-';
+            id += n;
+            id += '">';
+            return id;
+        }
+        function creationimgOurs(n){
+            img = '<img src="';
+            img += array[n].imageUrl;
+            img += '" alt="';
+            img += array[n].description;
+            img += '" class="img-ours">';
+            return img;
+        }
+
+        function creationnameOurs(n){
+            nom = '<h2 class="card-title text-center">';
+            nom += array[n].name;
+            nom += '</h2>';
+            return nom;
+        }
+
+        function creationDivOurs(n){
+            let ours="ours";
+            divOurs += '<a class="card"';
+            divOurs += creationHREF(ours, n);
+            divOurs += creationID(ours, n);
+            divOurs += creationimgOurs(n);
+            divOurs += creationnameOurs(n);
+            divOurs += '</a>';
+        }
+        function incrementationOurs(nombreDOurs,lengthOurs){
+            let n = 0;
+            while (nombreDOurs <= lengthOurs){
+                creationDivOurs(n);
+                nombreDOurs++;
+                n++;
+            }
+        }
+        
+        incrementationOurs(nombreDOurs, lengthOurs);
+        document.getElementById("row1").innerHTML = divOurs;
+
+    })
